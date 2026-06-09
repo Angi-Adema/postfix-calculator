e9242633a8361974cbe0837e8dabc35193d8344f
Angi-Adema <dr.adema@gmail.com>
2026-06-09 13:31:49 -0600

    see commit above, just saved.

--- src/postfixCalculator/PostfixException.java
+++ src/postfixCalculator/PostfixException.java
@@ -1,5 +1,12 @@
 package postfixCalculator;
 
-public class PostfixException {
-
+// Implement a custom exception class for displaying error messages
+public class PostfixException extends Exception {
+	
+	// Implement method that takes error message from PostfixCalculator methods
+	public PostfixException(String message) {
+		
+		// Call super to pass correct message from PostfixCalculator
+		super(message);
+	}
 }
